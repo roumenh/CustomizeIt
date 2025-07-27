6f7b5278647dba283f92f506ef4a4f0c1440b296
Roman Hruska <hruska.r@gmail.com>
2025-07-27 13:40:49 +0200

    Add Koin dependency, add Readme

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,8 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(project.dependencies.platform(libs.koin.bom))
+    implementation(libs.koin.core)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)

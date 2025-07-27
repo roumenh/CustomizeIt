837763fb18714f6c0915c323e3b2fbd9b4d44188
Roman Hruska <hruska.r@gmail.com>
2025-07-27 15:32:17 +0200

    Add Retrofit + moshi + converter + base for Api Service

---
+++ app/src/main/java/com/humanroman/customizeit/network/DemoApiService.kt
@@ -0,0 +1,28 @@
+package com.humanroman.customizeit.network
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+private const val BASE_URL = "https://humanroman.com/backends/customizeit/"
+
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
+    .baseUrl(BASE_URL)
+    .build()
+
+object DemoApi {
+    val retrofitService: DemoApiService by lazy {
+        retrofit.create(DemoApiService::class.java)
+    }
+}
+
+interface DemoApiService {
+
+}
+
